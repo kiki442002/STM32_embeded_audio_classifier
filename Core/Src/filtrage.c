41e6ecbf76d93b5eef1e846a279bbb2ed537789e
Killian <picot.killian@outlook.fr>
2024-11-15 20:30:29 +0100

    Ajout de la fonction Hamming_window et des constantes associées. Modification de l'inclusion de fichier pour utiliser hanningWindow.h.

--- Core/Src/filtrage.c
+++ Core/Src/filtrage.c
@@ -1,7 +1,7 @@
 
 #include "filtrage.h"
 #include "../../Drivers/CMSIS/DSP/Include/arm_math.h"
-#include "fir_coef.h"
+#include "hanningWindow.h"
 
 uint8_t StereoToMono(int16_t *pOut, int16_t *pIn, uint32_t size)
 {
@@ -40,4 +40,26 @@ uint8_t Hanning_window(int16_t *pOut, int16_t *pIn, uint32_t size, uint8_t signa
         pOut[i] = (int16_t)(hanningWindow[i] * (float)pIn_tmp[i]);
     }
     return HANNING_FILTER_OK;
+}
+
+uint8_t Hamming_window(int16_t *pOut, int16_t *pIn, uint32_t size, uint8_t signal_input_type)
+{
+
+    if (pIn == NULL || pOut == NULL)
+    {
+        return HAMMING_FILTER_ERROR;
+    }
+    int16_t *pIn_tmp = pIn;
+
+    if (signal_input_type == STEREO)
+    {
+        int16_t tmpSteroBff[size];
+        StereoToMono(tmpSteroBff, pIn, size * 2);
+        pIn_tmp = tmpSteroBff;
+    }
+    for (uint32_t i = 0; i < size; i++)
+    {
+        pOut[i] = (int16_t)(hammingWindow[i] * (float)pIn_tmp[i]);
+    }
+    return HAMMING_FILTER_OK;
 }
