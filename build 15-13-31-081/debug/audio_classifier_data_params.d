2d318dcffd81ec45b033b6a35998abb2bc4769b5
Killian <picot.killian@outlook.fr>
2025-01-27 02:46:37 +0100

    ok

---
+++ build 15-13-31-081/debug/audio_classifier_data_params.d
@@ -0,0 +1,6 @@
+build/debug/audio_classifier_data_params.o: \
+ X-CUBE-AI/App/audio_classifier_data_params.c \
+ X-CUBE-AI/App/audio_classifier_data_params.h \
+ Middlewares/ST/AI/Inc/ai_platform.h
+X-CUBE-AI/App/audio_classifier_data_params.h:
+Middlewares/ST/AI/Inc/ai_platform.h:
---
+++ build 15-13-31-081/debug/ov9655.d
@@ -0,0 +1,5 @@
+build/debug/ov9655.o: Drivers/BSP/Components/ov9655/ov9655.c \
+ Drivers/BSP/Components/ov9655/ov9655.h \
+ Drivers/BSP/Components/ov9655/../Common/camera.h
+Drivers/BSP/Components/ov9655/ov9655.h:
+Drivers/BSP/Components/ov9655/../Common/camera.h:
